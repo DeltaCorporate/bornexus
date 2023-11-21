47124418e62751261063d95cc7622884476b8883
Ahmindache Jawad <jawad.ahmindache@gmail.com>
2023-11-21 16:18:01 +0100

    Session entity

--- src/Entity/Users.php
+++ src/Entity/Users.php
@@ -10,7 +10,7 @@ use Symfony\Component\Security\Core\User\UserInterface;
 #[ORM\HasLifecycleCallbacks]
 #[ORM\Entity(repositoryClass: UserRepository::class)]
 #[ORM\Table(name: '`user`')]
-class User implements UserInterface, PasswordAuthenticatedUserInterface
+class Users implements UserInterface, PasswordAuthenticatedUserInterface
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
