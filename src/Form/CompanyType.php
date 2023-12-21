c62c2b9b857fba18c7bfab426f0fda1cdd39ef95
Houmame LAZAR <houms937@gmail.com>
2023-12-21 12:37:42 +0100

    company crud

---
+++ src/Form/CompanyType.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Company;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class CompanyType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name')
+            ->add('description')
+            ->add('siret')
+            ->add('zip')
+            ->add('address')
+            ->add('country')
+            ->add('website')
+            ->add('paypal_id')
+            ->add('stripe_id')
+            ->add('iban')
+            ->add('tva')
+            ->add('tva_reason')
+            ->add('created_at')
+            ->add('updated_at')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Company::class,
+        ]);
+    }
+}
