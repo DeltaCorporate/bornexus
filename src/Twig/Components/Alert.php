92b7a8ba572ea59057a0fb879826662c1378b78d
Ahmindache Jawad <jawad.ahmindache@gmail.com>
2023-12-24 17:11:02 +0100

    Alert component

--- src/Twig/Components/Alert.php
+++ src/Twig/Components/Alert.php
@@ -3,25 +3,23 @@ namespace App\Twig\Components;
 
 use Symfony\UX\TwigComponent\Attribute\AsTwigComponent;
 
-#[AsTwigComponent(template: 'components/button.html.twig')]
-class Button
+#[AsTwigComponent(template: 'components/alert.html.twig')]
+class Alert
 {
     
     public function __construct(
         public string $text = '',
         public string $icon = '',
         public string $color = 'primary',
-        public string $textColor = 'text-text',
         public string $size = 'base',
+        public string $textColor = 'text-text',
         public string $borderWidth = '0',
         public string $fontSize = 'md',
         public string $weight = 'font-medium',
         public string $radius = 'rounded-md',
-        public string $balise = 'button',
         /**
          * @var 'solid' | 'outline'
          */
-        public string $variant = 'solid',
         public array $attributes = []
  
     ) {
