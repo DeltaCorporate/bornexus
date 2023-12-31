8417a7dc680ad407812797573d20214be88accc2
Ahmindache Jawad <jawad.ahmindache@gmail.com>
2023-12-31 23:22:08 +0100

    rounded icon component
    Signed-off-by: Ahmindache Jawad <jawad.ahmindache@gmail.com>

---
+++ src/Twig/Components/RoundedIcon.php
@@ -0,0 +1,20 @@
+<?php
+namespace App\Twig\Components;
+
+use Symfony\UX\TwigComponent\Attribute\AsTwigComponent;
+
+#[AsTwigComponent(template: 'components/roundedIcon.html.twig')]
+class RoundedIcon
+{
+    
+    public function __construct(
+       public string $icon = '',
+       public string $bgColor = 'base',
+       public string $color = 'neutral',
+       public string $size = '12',
+       public string $fontSize = '2xl'
+    ) {
+    }
+
+   
+}
