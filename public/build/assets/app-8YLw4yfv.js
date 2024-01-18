bcf0542c3bd5da4bc14b7a7b8d2f4a5105e2b99c
Houmame LAZAR <houms937@gmail.com>
2024-01-18 08:31:06 +0100

    remove useless docker

--- public/build/assets/app-8YLw4yfv.js
+++
@@ -1,1 +0,0 @@
-window.togglePassword=function(e){let s=e.parentElement.querySelector("input");e.classList.toggle("fa-eye-slash"),e.classList.toggle("fa-eye"),s.type=s.type==="password"?"text":"password"};
