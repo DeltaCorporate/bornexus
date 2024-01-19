4ad242fb0597f5bc8545c3b94c63e2e5a916ab8f
Houmame LAZAR <houms937@gmail.com>
2024-01-19 13:47:37 +0100

    roles roles hierarchy controllers move

---
+++ config/menu.php
@@ -0,0 +1,93 @@
+<?php
+return [
+    [
+        "route" => "",
+        "route_args" => [],
+        "title" => "Général",
+        "icon" => "fa fa-cog",
+        "role" => \App\Enum\RoleEnum::ROLE_SUPER_ADMIN,
+    ],
+    [
+        "route" => "",
+        "route_args" => [],
+        "title" => "Facturation",
+        "icon" => "fa fa-calculator",
+        "role" => \App\Enum\RoleEnum::ROLE_COMMERCIAL_COMPANY,
+        "subMenu" => [
+            [
+                "route" => "commercial_company_app_billing_index",
+                "route_args" => [],
+                "title" => "Devis",
+                "icon" => "fa fa-gear",
+                "role" => \App\Enum\RoleEnum::ROLE_COMMERCIAL_COMPANY,
+            ],
+            [
+                "route" => "",
+                "route_args" => [],
+                "title" => "Factures",
+                "icon" => "fa fa-gear",
+                "role" => \App\Enum\RoleEnum::ROLE_COMMERCIAL_COMPANY,
+            ]
+        ]
+    ],
+    [
+        "route" => "",
+        "route_args" => [],
+        "title" => "Comptabilité",
+        "icon" => "fa fa-calculator",
+        "role" => \App\Enum\RoleEnum::ROLE_ACCOUNTANT_COMPANY,
+    ],
+    [
+        "route" => "",
+        "route_args" => [],
+        "title" => "Gestion",
+        "icon" => "fa fa-cog",
+        "role" => \App\Enum\RoleEnum::ROLE_ADMIN_COMPANY,
+        "subMenu" => [
+            [
+                "route" => "",
+                "route_args" => [],
+                "title" => "Clients",
+                "icon" => "fa fa-gear",
+                "role" => \App\Enum\RoleEnum::ROLE_COMMERCIAL_COMPANY,
+            ],
+            [
+                "route" => "",
+                "route_args" => [],
+                "title" => "Fournisseurs",
+                "icon" => "fa fa-gear",
+                "role" => \App\Enum\RoleEnum::ROLE_COMMERCIAL_COMPANY,
+            ]
+        ]
+    ],
+    [
+        "route" => "",
+        "route_args" => [],
+        "title" => "Produits",
+        "icon" => "fa fa-cog",
+        "role" => \App\Enum\RoleEnum::ROLE_COMMERCIAL_COMPANY,
+        "subMenu" => [
+            [
+                "route" => "",
+                "route_args" => [],
+                "title" => "Produits",
+                "icon" => "fa fa-gear",
+                "role" => \App\Enum\RoleEnum::ROLE_COMMERCIAL_COMPANY,
+            ],
+            [
+                "route" => "",
+                "route_args" => [],
+                "title" => "Catégories",
+                "icon" => "fa fa-gear",
+                "role" => \App\Enum\RoleEnum::ROLE_COMMERCIAL_COMPANY,
+            ]
+        ]
+    ],
+    [
+        "route" => "",
+        "route_args" => [],
+        "title" => "Paramètres",
+        "icon" => "fa fa-gear",
+        "role" => \App\Enum\RoleEnum::ROLE_ADMIN_COMPANY,
+    ]
+];
--- src/Twig/Components/Sidebar.php
+++ src/Twig/Components/Sidebar.php
@@ -2,6 +2,8 @@
 // src/Twig/Components/Sidebar.php
 namespace App\Twig\Components;
 
+use App\Enum\RoleEnum;
+use Symfony\Component\Security\Core\Authorization\AuthorizationCheckerInterface;
 use Symfony\UX\TwigComponent\Attribute\AsTwigComponent;
 use Symfony\Component\HttpKernel\KernelInterface;
 
@@ -20,31 +22,34 @@ class Sidebar
     public float $logoSizeRem;
 
     public string $logo;
-    private $menuItems;
-    private $twig;
-    
-    public function __construct(KernelInterface $kernel, Environment $twig){
+    private array $menuItems;
+
+    public function __construct(KernelInterface $kernel,private readonly Environment $twig,private readonly AuthorizationCheckerInterface $authorizationChecker){
         $projectDir = $kernel->getProjectDir();
-        $this->twig = $twig;
-        $this->menuItems = json_decode(file_get_contents($projectDir.'/config/menu.json'));
+        $this->menuItems = require $projectDir.'/config/menu.php';
+//        dd($this->menuItems);
     }
-    public function generateMenuItems(array $menuItems = null,bool $isDropdown = false){
+    public function generateMenuItems(array $menuItems = null,bool $isDropdown = false): void
+    {
             
-            $menuItems = $menuItems ?? $this->menuItems;
-        
-            foreach($menuItems as $menuItem){
-                $menuItem = (array)$menuItem;
-            
-            if(isset($menuItem['subMenu'])){
-                $menuItem['sidebar'] = $this;
-                echo $this->twig->render('components/Sidebar/menuDropdown.html.twig',$menuItem); 
+        $menuItems = $menuItems ?? $this->menuItems;
+        foreach($menuItems as $menuItem){
+            $menuItem = (array)$menuItem;
+
+            $role = $menuItem['role'] ?? RoleEnum::ROLE_USER;
+            if($this->authorizationChecker->isGranted($role->name)){
+                if(isset($menuItem['subMenu'])){
+                    $menuItem['sidebar'] = $this;
+                    echo $this->twig->render('components/Sidebar/menuDropdown.html.twig',$menuItem);
+                }
+                else{
+                    $menuItem['textColor'] = !$isDropdown ? $this->textColor : $this->textColorDropdown;
+                    $menuItem['hoverColor'] = $this->hoverColor;
+
+                    echo $this->twig->render('components/Sidebar/menuItem.html.twig',$menuItem);
+                }
             }
-            else{
-                $menuItem['textColor'] = !$isDropdown ? $this->textColor : $this->textColorDropdown;
-                $menuItem['hoverColor'] = $this->hoverColor;
-                
-                echo $this->twig->render('components/Sidebar/menuItem.html.twig',$menuItem); 
-            }
+
         }
     }
 
