cfa5a0ce3afdb4ac645ffdd5ed6784a4880d86a1
Ahmindache Jawad <jawad.ahmindache@gmail.com>
2024-02-08 12:21:04 +0100

    delete billings + show invoices
    Signed-off-by: Ahmindache Jawad <jawad.ahmindache@gmail.com>

--- src/Repository/BillingsRepository.php
+++ src/Repository/BillingsRepository.php
@@ -3,7 +3,9 @@
 namespace App\Repository;
 
 use App\Entity\Billing;
+use App\Entity\BillingCompanyCatalog;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\Persistence\ManagerRegistry;
 
 /**
@@ -16,7 +18,9 @@ use Doctrine\Persistence\ManagerRegistry;
  */
 class BillingsRepository extends ServiceEntityRepository
 {
-    public function __construct(ManagerRegistry $registry)
+    public function __construct(
+      private  EntityManagerInterface $entityManager,
+      private ManagerRegistry $registry)
     {
         parent::__construct($registry, Billing::class);
     }
@@ -27,5 +31,16 @@ class BillingsRepository extends ServiceEntityRepository
             ->orderBy('b.id', 'ASC')
             ->getQuery();
     }
-        
+
+    public function delete(Billing $billing): void{
+
+        $catalogs = $this->entityManager->getRepository(BillingCompanyCatalog::class)->findBy(['billing' => $billing]);
+        foreach ($catalogs as $catalog)
+            $this->entityManager->remove($catalog);
+
+
+        $this->entityManager->remove($billing);
+        $this->entityManager->flush();
+
+    }
 }
