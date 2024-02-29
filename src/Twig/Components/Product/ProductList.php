505fc9e52ec8bb56482467492f2664dd759ca129
Houmame LAZAR <houms937@gmail.com>
2024-02-29 12:28:17 +0100

    fix product problems on file upload and fav upload

--- src/Twig/Components/Product/ProductList.php
+++ src/Twig/Components/Product/ProductList.php
@@ -2,8 +2,11 @@
 
 namespace App\Twig\Components\Product;
 
+use App\Entity\User;
 use App\Repository\ProductsRepository;
 use Symfony\UX\LiveComponent\Attribute\AsLiveComponent;
+use Symfony\UX\LiveComponent\Attribute\LiveAction;
+use Symfony\UX\LiveComponent\Attribute\LiveProp;
 use Symfony\UX\LiveComponent\DefaultActionTrait;
 
 #[AsLiveComponent]
@@ -13,10 +16,27 @@ final class ProductList
     public array $products = [];
     public int $totalProducts = 0;
 
+    #[LiveProp(writable: true)]
+    public array $favProducts = [];
+
     public function __construct(ProductsRepository $productsRepository)
     {
         //find all products sorted by created_at
         $this->products = $productsRepository->findBy([], ['createdAt' => 'DESC']);
         $this->totalProducts = $productsRepository->count([]);
+    }
+
+    /*
+     * Tableau en base de données (ancien tableau)
+     * Tableau fait par le user (nouveau tableau)
+     * Si dans ancien tableau mais pas dans le nouveau, on supprime
+     * Si dans nouveau tableau mais pas dans l'ancien, on ajoute
+     * Si dans les deux, on ne fait rien
+     * */
+
+    #[LiveAction]
+    public function favoriteProduct(): void
+    {
+        dd($this->favProducts);
     }
 }
